f7620e856851d4f92949f301525c1a104ac14c6f
Aditya Kumar <aryan68125@gmail.com>
2023-04-24 19:27:37 +0530

    integer

---
+++ php_beginner/integer.php
@@ -0,0 +1,23 @@
+<?php
+/* Integer */
+$x = 5; //integer as base 10 represtation
+$y = 0x2A; //integer as hexadecimal number or base 16 representation
+$z = 055; //integers as octal numbers
+$a = 0b111; //integers as binary numbers
+echo "$x integer as base 10 decimal";
+echo "<br/>";
+echo "$y integer as hexadecimal base 16";
+echo "<br/>";
+echo "$z integer as octal numbers";
+echo "<br/>";
+echo "$a integer as binary number";
+echo "<br/>";
+/*Integer overflow */
+echo "integer overflow";
+echo "<br/>";
+$k = PHP_INT_MAX;
+var_dump($k);
+echo "<br/>";
+$k=$k+1; //when integer overflow happens it will automatically gets converted to float
+var_dump($k);
+echo "<br/>";
